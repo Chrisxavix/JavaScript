f1a0322294ff572cf44ee8d4ffef080e69d58302
Chris <chrischillogallid64@outlook.com>
2020-09-15 21:05:23 -0500

    chore: clases, console, set, get, prototipo

--- main.js
+++ main.js
@@ -918,95 +918,360 @@ Animal {nombre: "Mal", genero: "Macho"}
 Animal {nombre: "Dana", genero: "Hembra"}
 Hacer sonido del animal
 Hola, me llamo Dana */
-
 /* ----------------------- Prototipos ----------------------- */
 
+/* ---------------------- Herencia ------------------------------ */
 
+/* Heredar características de un padre al hijo 
+El "super" manda a llamar el contructor del padre */
 
+/* function Animal (nombre, genero) {
+    // Atributos
+    this.nombre = nombre;
+    this.genero = genero;
+}
+// Métodos
+Animal.prototype.sonar = function () {
+    console.log('Hacer sonido del animal');
+}
+Animal.prototype.saludar = function () {
+    console.log('Hola, me llamo', this.nombre);
+}
+function Perro (nombre, genero, tamanio) {
+    this.super = Animal;
+    this.super(nombre, genero)
+    this.tamanio = tamanio;
+}
+// Perro está heredando de Animal
+Perro.prototype = new Animal();
+Perro.prototype.constructor = Perro;
+// Sobreescritura del método del prototipo padre en el hijo
+Perro.prototype.sonar = function () {
+    console.log('Perross');
+}
+Perro.prototype.ladrar = function() {
+    console.log('guuuuuua');
+}
+const snoop = new Perro('Mal', 'Macho', 'Mediano');
+const snoopy = new Animal('Dana', 'Hembra');
+console.log(snoop); */
+/* Imprime: 
+genero: "Macho"
+nombre: "Mal"
+super: ƒ Animal(nombre, genero)
+tamanio: "Mediano"
+__proto__: Animal */
+/* ---------------------- Herencia ------------------------------ */
 
+/* ------------------------ Clases y Herencia ----------------------------- */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* ---------------- Ejercicios ------------------- */
-
-
-
-/* https://informaticapc.com/tutorial-javascript/math.php#:~:text=sqrt()%3A%20devuelve%20la%20ra%C3%ADz%20cuadrada%20del%20n%C3%BAmero%20pasado%20como%20par%C3%A1metro. */
-/* console.log( Math.max(43,5,7)); */
-
-
-
-/* Pares e impares */
-
-/* const valor = prompt('Ingrese:')
-const numeros = [valor];
-
-for(let i = 0; i < numeros.length; i++) {
-    if (numeros[i]%2 === 1) {
-        console.log(numeros[i], 'Impares')
-    } else {
-        console.log(numeros[i], 'Pares')
+/* Desde el 2015 se puede usar clases, el azucar sintático.
+A pesar de usar clases, JavaScript lo tomará como funciones
+contructoras en la ejecución. */
+/* ------------ Clase ------------ */
+/* class Animal {
+    constructor(nombre, genero) {
+    // Atributos
+    this.nombre = nombre;
+    this.genero = genero;   
     }
-} */
-
-
-
-
-/* Pares e impares */
-/* const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-for(let i = 0; i < numeros.length; i++) {
-    if (numeros[i]%2 === 1) {
-        console.log(numeros[i], 'Impares')
-    } else {
-        console.log(numeros[i], 'Pares')
+    // Métodos
+    sonar() {
+        console.log('Hacer sonido del animal');
+    }
+    saludar() {
+        console.log('Hola, me llamo', this.nombre);
     }
 }
- */
+const mimi = new Animal('Mimi', 'Hembra');
+const scooby = new Animal('Scooby', 'Macho');
+mimi.sonar();
+mimi.saludar();
+console.log(mimi);
+console.log(scooby); */
+/* Imprime:1
+genero: "Hembra"
+nombre: "Mimi"
+__proto__:
+constructor: class Animal
+saludar: ƒ saludar()
+sonar: ƒ sonar()
+__proto__: Object
+Imprime2:  
+genero: "Macho"
+nombre: "Scooby"
+__proto__:
+constructor: class Animal
+saludar: ƒ saludar()
+sonar: ƒ sonar()
+__proto__: Object 
+Imprime3: 
+Hacer sonido del animal
+Imprime4:
+Hola, me llamo Mimi */
 
+/* ------------ Herencia ------------ */
+/* class Animal {
+    constructor(nombre, genero) {
+    // Atributos
+    this.nombre = nombre;
+    this.genero = genero;   
+    }
+    // Métodos
+    sonar() {
+        console.log('Hacer sonido del animal');
+    }
+    saludar() {
+        console.log('Hola, me llamo', this.nombre);
+    }
+}
+class Perro extends Animal {
+    constructor(nombre, genero, tamanio) {
+// Super manda a llamar al constructor de la clase Padre (Animal)
+        super (nombre, genero);
+// Atributo
+    this.tamanio = tamanio;
+    }
+    sonar(){
+        console.log('Soy un perro');
+    }
+    ladrar(){
+        console.log('guuuuuuuuuuuao');
+    }
+}
+const mimi = new Animal('Mimi', 'Hembra');
+const scooby = new Perro('Scooby', 'Macho', 'Cazador');
+console.log(mimi);
+mimi.sonar();
+mimi.saludar();
+console.log(scooby);
+scooby.sonar();
+scooby.saludar();
+scooby.ladrar(); */
+/* Imprimir1: 
+Animal {nombre: "Mimi", genero: "Hembra"}
+genero: "Hembra"
+nombre: "Mimi"
+__proto__: 
+Imprimir2:
+Hacer sonido del animal
+Imprimir3:
+Hola, me llamo Mimi
+Imprimir4:
+Perro {nombre: "Scooby", genero: "Macho", tamanio: "Cazador"}
+genero: "Macho"
+nombre: "Scooby"
+tamanio: "Cazador"
+__proto__: Animal 
+Imprimir5:
+Soy un perro 
+Imprimir6:
+Hola, me llamo Scooby 
+Imprimir7:
+guuuuuuuuuuuao */
 
+/* Ejercicio de clases y herencia, 1*/
+/* class Datos {
+    constructor (num1, num2) {
+        this.num1 = num1
+        this.num2 = num2
+    }
+}
+class Suma extends Datos {
+    constructor (num1, num2, respuesta) { // Constructor agrega más parámetros en caso de necesitar
+        super(num1, num2)
+        this.respuesta = respuesta;
+    }
+    operar () {
+        this.respuesta = this.num1 + this.num2;
+        console.log(`${this.num1} + ${this.num2} = ${this.respuesta}`)
+    }
+} 
+const suma = new Suma(80,20)
+suma.operar() */
+/* Imprime: 
+80 + 20 = 100 */
 
-/* console.log("Números pares entre el 0 y 20");
-    let numPares;
-    for (let numPares = 0; numPares <= 20; numPares++) {
-        if (numPares % 2 == 0) {
-            console.log(numPares);
+/* Ejercicio de clases y herencia, 2*/
+/* class Datos {
+    constructor (num1, num2) {
+        this.num1 = num1
+        this.num2 = num2
+    }
+}
+class Operaciones extends Datos {
+    constructor (num1, num2, operador) {
+        super(num1, num2);
+        this.operador = operador;
+    }
+    operar () {
+        let respuesta;
+        if(this.operador === "+") {
+            respuesta = this.num1 + this.num2;
+            console.log(`${this.num1} ${this.operador} ${this.num2} = ${respuesta}`)
+        } else if(this.operador === "-"){
+            respuesta = this.num1 - this.num2;
+            console.log(`${this.num1} ${this.operador} ${this.num2} = ${respuesta}`)
+        } else if(this.operador === "*"){
+            respuesta = this.num1 * this.num2;
+            console.log(`${this.num1} ${this.operador} ${this.num2} = ${respuesta}`)
+        } else if(this.operador === "/"){
+            let respuesta = this.num1 / this.num2;
+            console.log(`${this.num1} ${this.operador} ${this.num2} = ${respuesta}`)
         }
-    } */
+    }
+} 
+const suma = new Operaciones(10,20,"+")
+const resta = new Operaciones(30,20,"-")
+const multi = new Operaciones(8,2,"*")
+const div = new Operaciones(10,2,"/")
+suma.operar()
+resta.operar()
+multi.operar()
+div.operar() */
+/* Imprime:
+10 + 20 = 30
+30 - 20 = 10
+8 * 2 = 16
+10 / 2 = 5 */
+/* ------------------------ Clases y Herencia ----------------------------- */
 
-/* console.log("Números de 5 en 5 hasta 20");
-    let num;
-    for (let num = 0; num <= 20; num+=5) {
-        console.log(num);
-    } */
+/* -------- Métodos estáticos, getters y setters -------------- */
 
-/* console.log("Escribir Par o Impar");
-    let contador;
-    for (let contador = 1; contador <= 10; contador++) {
-        if (contador % 2 == 0) {
-            console.log(contador + " es PAR");
-        }else{
-            console.log(contador + " es IMPAR");
-        }
-    } */
+/* ----------- Métodos estáticos */
+/* Un método estático estático es aquel que se puede ejecutar sin la
+necesidad de instanciar la clase. El método (queEres()) */
+
+/* class Animal {
+    constructor(nombre, genero) {
+    // Atributos
+    this.nombre = nombre;
+    this.genero = genero;   
+    }
+    // Métodos
+    sonar() {
+        console.log('Hacer sonido del animal');
+    }
+    static saludar() {
+        console.log('Hola, me llamo', this.nombre);
+    }
+}
+class Perro extends Animal {
+    constructor(nombre, genero, tamanio) {
+// Super manda a llamar al constructor de la clase Padre (Animal)
+        super (nombre, genero);
+// Atributo
+    this.tamanio = tamanio;
+    }
+    sonar(){
+        console.log('Soy un perro');
+    }
+    ladrar(){
+        console.log('guuuuuuuuuuuao');
+    }
+    static queEres(){
+        console.log('Es un animal Perro');
+    }
+}
+// Sin crear un objeto puedo invocar el método estático 
+Perro.queEres(); */
+/* Imprime: 
+Es un animal Perro*/
+
+/* ------------- Getters y setters */
+/* Los setters y getters son métodos especiales que nos
+permiten establecer y obtener los valores de atributos
+nuestra clase */
+
+/* class Animal {
+    constructor(nombre, genero) {
+    // Atributos
+    this.nombre = nombre;
+    this.genero = genero;   
+    }
+    // Métodos
+    sonar() {
+        console.log('Hacer sonido del animal');
+    }
+    static saludar() {
+        console.log('Hola, me llamo', this.nombre);
+    }
+}
+class Perro extends Animal {
+    constructor(nombre, genero, tamanio) {
+// Super manda a llamar al constructor de la clase Padre (Animal)
+        super (nombre, genero);
+// Atributo
+    this.tamanio = tamanio;
+// Al crear la nueva variable raza, no lo defino como
+// parámetro del constructor, por eso lo mando en null
+    this.raza = null;
+    }
+    sonar(){
+        console.log('Soy un perro');
+    }
+    ladrar(){
+        console.log('guuuuuuuuuuuao');
+    }
+    static queEres(){
+        console.log('Es un animal Perro');
+    }
+// Método especial para obtener el valor
+    get getraza(){
+        return this.raza;
+        
+    }
+
+    set setraza(raza){
+        this.raza = raza;
+    }
+}
+const scooby = new Perro('Scooby', 'Macho', 'Cazador');
+console.log(scooby.getraza);
+scooby.setraza = 'Demons';
+console.log(scooby.getraza); */
+/* Imprime:  
+null
+Demons */
+
+/* El guion bajo en el id (this._name) se coloca por ser
+privada */
+/* class Persona {
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+}
+
+const person = new Persona();
+person.name = 'Chris';
+console.log(person.name); */
+/* Imprime: 
+Chris */
+/* -------- Métodos estáticos, getters y setters -------------- */
+
+
+/* ------------------ Objeto console ------------------------- */
+
+/* console.error('Esto es un error'); */
+/* Imprime: (color rojo)
+Esto es un error */
+
+/* console.warn('Esto es un aviso'); */
+/* Imprime: (color amarillo)
+Esto es un aviso */
+
+/* console.log('Esto es un registro de lo que pasa'); */
+/* Imprime: (color amarillo)
+Esto es un registro de lo que pasa */
+
+/* console.clear(); */
+/* Imprime:
+Console was cleared */
+
+
+/* ------------------ Objeto console ------------------------- */
+
