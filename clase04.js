67cd1f7239a58b7bddb69755d7a89df6974a2e8e
chris <cxchillogalli@engicoders.com>
2020-09-17 12:19:08 -0500

    chore: expresiones regulares

--- clase04.js
+++ clase04.js
@@ -1,9 +1,10 @@
 /* --------------------- CURSO JAVASCRIPT ------------------- */
 /* 
-1) Prototipos: Línea 9
-2) Objeto Math: Linea 28
-3) Objeto Date: Línea 56
-4) Operador Cortocircuito: Línea 100
+1) Prototipos: Línea 10
+2) Objeto Math: Linea 29
+3) Objeto Date: Línea 57
+4) Operador Cortocircuito: Línea 101
+5) Expresiones regulares: Línea 149
 */
 
 /* 1) ---------------- Objeto console ------------------ */
@@ -144,3 +145,39 @@ false
 Imprime2:
 Izquierda */
 /* ----------------- Operador Cortocircuito -------------- */
+
+/* 5) ----------- Expresiones regulares ----------------- */
+
+/* Secuencia de caracteres que forman un patrón de búsqueda,
+principalmente en la búsqueda de cadenas de texto 
+la (g) es búsqueda global, la (i) ignora mayúscylas*/
+
+/* let cadena = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis velit assumenda dolorem at autem, fugiat quia dicta sint aperiam facere accusantium nam! Enim, eum. Error, molestias soluta. Delectus, asperiores illum."
+let expReg = new RegExp("lorem", "g");
+let expReg2 = /lorem/ */
+/* buscamos la expresión dentro de cadena con (test) */
+/* console.log(expReg.test(cadena)); */
+/* Imprime:
+true */
+/* Devuelve un arreglo (exec) */
+/* console.log(expReg.exec(cadena)); */
+/* Imprime: tener comentado el console de test
+["lorem", index: 84, input: "Lorem, ipsum dolor 
+sit amet consectetur adipisicin…or, molestias soluta. 
+Delectus, asperiores illum.", groups: undefined] */
+
+
+/* let cadena = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis velit assumenda dolorem at autem, fugiat quia dicta sint aperiam facere accusantium nam! Enim, eum. Error, molestias soluta. Delectus, asperiores illum."
+let expReg = new RegExp("lorem", "i");
+let expReg2 = /lorem/ */
+/* buscamos la expresión dentro de cadena con (test) */
+/* console.log(expReg.test(cadena)); */
+/* Imprime:
+true */
+/* Devuelve un arreglo (exec) */
+/* console.log(expReg.exec(cadena)); */
+/* Imprime: no hace falta tener comentado el console de test
+["lorem", index: 84, input: "Lorem, ipsum dolor 
+sit amet consectetur adipisicin…or, molestias soluta. 
+Delectus, asperiores illum.", groups: undefined] */
+/* 5) ----------- Expresiones regulares ----------------- */
