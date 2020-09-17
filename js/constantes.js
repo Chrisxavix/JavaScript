c4770768bee548bf434f0d368e601f18eec0be33
chris <cxchillogalli@engicoders.com>
2020-09-17 16:27:00 -0500

    chore: modulos

---
+++ js/constantes.js
@@ -0,0 +1,14 @@
+/* PI y usuario son exports que serán llamados con import
+en el archivo módulo, por cada variable */
+/* Code: 001 */
+/* export const PI = Math.PI;
+export let usuario = 'Chris';
+let password = 'key'; */
+
+/* Exporta la clase */
+/* Code: 004 */
+/* export class Saludar {
+    constructor() {
+        console.log("Hoooola");
+    }
+} */
