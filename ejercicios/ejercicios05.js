e061eafb14dc7ecd0f8053bce1652c33f6af3500
chris <cxchillogalli@engicoders.com>
2020-09-24 15:09:57 -0500

    chore: ejer05

---
+++ ejercicios/ejercicios05.js
@@ -0,0 +1,36 @@
+/* -------- Ejercicios JavaScript ---------- */
+
+/* 15) Programa una función para convertir números de base
+binaria a decimal y viceversa, pe. miFuncion(100,2) 
+devolverá 4 base 10.
+
+16) Programa una función que devuelva el monto final
+después de aplicar un descuento a una cantidad dada, 
+pe. miFuncion(1000, 20) devolverá 800.
+
+17) Programa una función que dada una fecha válida 
+determine cuantos años han pasado hasta el día de hoy,
+pe. miFuncion(new Date(1984,4,23)) devolverá 35 años
+(en 2020). */
+
+/* ----------------- DESARROLLO -------------------- */
+
+/* 15) ------------------------------------------------- */
+/* const entrada = () => {
+    let numero = parseInt( prompt('Ingrese el valor'));
+    let base = parseInt(prompt('Ingrese la base'));
+    if (numero<=0) return alert('Solo números mayores a 1.');
+    if (base<1) return alert('Solo números mayores a 1.');
+    if (!base) return alert('No ingresó la base.');
+    if (base===2){
+        return alert(`${parseInt( numero)} base ${parseInt( base)} = ${parseInt(numero,base)} base`);
+    }
+}
+entrada(); */
+/* 15) ------------------------------------------------- */
+
+/* 16) ------------------------------------------------- */
+/* 16) ------------------------------------------------- */
+
+/* 17) ------------------------------------------------- */
+/* 17) ------------------------------------------------- */
--- extra.md
+++ extra.md
@@ -1,1 +1,4 @@
 https://github.com/nicksiuxs/CursoJavaScript/tree/master/Ejercicios
+
+https://www.youtube.com/watch?v=WbJzfthCFrM&list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA&index=39&ab_channel=jonmircha
+ejercicios
