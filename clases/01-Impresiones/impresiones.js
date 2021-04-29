6c838e8cf33be424a692b5adb5dc510d15060a08
chris <cxchillogalli@engicoders.com>
2021-04-29 15:21:31 -0500

    feat: impresiones

---
+++ clases/01-Impresiones/impresiones.js
@@ -0,0 +1,28 @@
+/* ------------------Tipos de impresiones ---------------- */
+
+/* console: imprime en la consola (F12: abre la consola del
+navegador)
+alert: notificación en el navegador
+prompt: ventana de ingreso de valores 
+document.write: imprime en la página */ 
+
+/* alert('hola'); */
+/* Imprime: con botón de aceptar
+Hola */
+
+/* Declarar una variable para recoger el valor ingresado */
+/* let aviso = prompt('Ingrese un dato');
+console.log(aviso); */
+/* Imprime: ventana de ingreso de datos
+Carlos */
+
+/* El confirm recoge el valor de (aceptar o cancelar) */
+/* let confir =confirm('hola');
+console.log(confir); */
+/* Imprime: con dos botones, aceptar = true 
+y cancelar = false */
+
+/* document.write("Hola"); */
+/* Imprime: en el template o página 
+Hola */
+/* ------------------Tipos de impresiones------------------- */
