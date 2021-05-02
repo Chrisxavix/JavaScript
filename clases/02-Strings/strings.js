284888279bc10130f8c5aa94e7e508534f4f6ecc
Chris <chrischillogallid64@outlook.com>
2021-05-01 22:34:40 -0500

    feat: edit strings

--- clases/02-Strings/strings.js
+++ clases/02-Strings/strings.js
@@ -1,84 +1,77 @@
 /* --------------------- Strings ------------------------- */
 
-/* 
-Al poner ',' se concatena las cadenas con variables
-Existe un espaciado extra
-*/
+/* Concatenar con la coma */
+/* Al poner ',' se concatena las cadenas con variables, existe un espaciado extra */
 console.log('--------- 1 ---------');
 const nombre1 = "Chris";
 console.log('Hola ',nombre1,'. Su nombre tiene: ',nombre1.length,' letras.');
 
-/* 
-Al poner '+' se concatena las cadenas con variables
-No existe un espaciado extra
-*/
+/* Concatenar con el signo + */
+/* Al poner '+' se concatena las cadenas con variables, no existe un espaciado extra */
 console.log('--------- 2 ---------');
 const nombre2 = "Chris";
 console.log('Hola '+nombre2+'. Su nombre tiene: '+nombre2.length+' letras.');
 
-/* 
-Al poner '`' se concatena las cadenas con variables
-Es lo recomendado a usar
-*/
+/* Concatenar con el signo ` */
+/* Al poner '`' se concatena las cadenas con variables, es lo recomendado a usar */
 console.log('--------- 3 ---------');
 const nombre3 = "Chris";
 console.log(`Hola ${nombre3}. Su nombre tiene: ${nombre3.length} letras.`);
 
-/* Test  -------------------------------------------------------------------------------------------------*/
-
-/* Convertir un texto a minúscula */
+/* toLowerCase(): convertir un texto a minúscula */
 console.log('--------- 4 ---------');
 const texto1 = 'MARIA';
 console.log(texto1.toLowerCase());
 
-/* Convertir un texto a mayúscula */
+/* toUpperCase(): convertir un texto a mayúscula */
 console.log('--------- 5 ---------');
 const texto2 = 'carlos';
 console.log(texto2.toUpperCase());
 
-/* Includes verifica que exista el string en la cadena y responde con true o false */
+/* includes(): verifica que exista el string en la cadena */
+/* Responde con true o false */
 console.log('--------- 6 ---------');
 const texto3 = 'lorem tres cuatro cinco';
 console.log(texto3.includes('cinco'));
 
-/* Elimina los espacios de inicio y fin con trim */
+/* trim(): elimina los espacios de inicio y fin */
 console.log('--------- 7 ---------');
 const texto4 = '  lorem tres cuatro cinco     ';
 console.log(texto4);
 console.log(texto4.trim());
 
-/* Separa las cadenas a partir un espacio y lo visualiza en un array con Split */
+/* split(): separa las cadenas a partir un valor */
+/* Devuelve un array con caracteres separados a partir del valor */
 console.log('--------- 8 ---------');
 const texto5 = 'lorem tres cuatro cinco';
 console.log(texto5.split(' '));
-
-/* Imprime el primer string despues del "." a partir de la posición */
-console.log('--------- 9 ---------');
+/* split('.')[1]: imprime el primer string despues del "." a partir de la posición */
+console.log('--------- 8.1 ---------');
 const texto6 = "Dani Fhn. Filth. Test. Juan"; 
 const splitTexto6 = texto6.split('.')[1];
 console.log(splitTexto6);
 
-/* Separa la cadena después de un punto e imprime separadas en un array con Split */
-console.log('--------- 10 ---------');
-const texto7 = "Dani.Chris"; 
-const splitTexto7 = texto7.split('.');
-console.log(splitTexto7);
-
-/* Imprime la primera letra de la cadena con Substring */
-console.log('--------- 11 ---------');
+/* substring(1): elimina la primera letra de la cadena */
+console.log('--------- 9 ---------');
+const texto10 = "Dani Fhn Filth asd, asddsf"; 
+const splitTexto10 = texto10.substring(1);
+console.log(splitTexto10);
+/* substring(0, 1): imprime la primera letra de la cadena con substring */
+console.log('--------- 9.1 ---------');
 const texto8 = "dani Fhn Filth asd, asddsf"; 
 const splitTexto8 = texto8.substring(0, 1);
 console.log(splitTexto8);
-
-/* Imprime la primera letra de la cadena y lo convierte a mayúscula */
-console.log('--------- 12 ---------');
+/* substring(0, 1).toUpperCase(): imprime la primera letra de la cadena y lo convierte a mayúscula */
+console.log('--------- 9.2 ---------');
 const texto9 = "dani Fhn Filth asd, asddsf"; 
 const splitTexto9 = texto9.substring(0, 1).toUpperCase();
 console.log(splitTexto9);
 
-/* Elimina la primera letra de la cadena con Substring */
-console.log('--------- 13 ---------');
-const texto10 = "Dani Fhn Filth asd, asddsf"; 
-const splitTexto10 = texto10.substring(1);
-console.log(splitTexto10);
+/* replace(): reemplaza un string por otro */
+/* Toma una parte del texto y lo reemplaza, dando una cadena de texto nueva */
+console.log('--------- 10 ---------');
+const arrayReplace = 'www.texto.com?id=3';
+const resultReplace = arrayReplace.replace('texto.', 'maggot-code.')
+console.log(arrayReplace, 'original');
+console.log(resultReplace, 'modificado');
 /* --------------------- Strings ------------------------- */
--- metodos/metodos.js
+++ metodos/metodos.js
@@ -1,23 +1,12 @@
-
-/* Método Replace */
-/* Agarra una parte del texto y lo reemplaza, dando una cadena de texto nueva */
-console.log('/******** Método Replace *********/');
-const arrayReplace = 'www.texto.com?id=3';
-const resultReplace = arrayReplace.replace('www.', 'test.')
-console.log(arrayReplace);
-console.log(resultReplace);
-/* Imprime:
-test.texto.com?id=3 */
-
 /* Método Map con objetos*/
 /* Busca el elemento en todos los arrays, y me devuelve un array con los
 elementos mapeados por el atributo esperado */
 const arrayMapaObject = [
-    {name: 'Chris', age: 12, lastname: 'Test01'},
-    {name: 'Dani', age: 12, lastname: 'Test02'},
-    {name: 'Xavi', age: 12, lastname: 'Test03'},
-    {name: 'Lucas', age: 12, lastname: 'Test04'}
-]
+    { name: 'Chris', age: 12, lastname: 'Test01' },
+    { name: 'Dani', age: 12, lastname: 'Test02' },
+    { name: 'Xavi', age: 12, lastname: 'Test03' },
+    { name: 'Lucas', age: 12, lastname: 'Test04' }
+];
 console.log('/******** Método Map con objetos *********/');
 const resultMapaObject = arrayMapaObject.map(elemn => {
     return elemn.name;
