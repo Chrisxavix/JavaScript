9fc9ce46f1432140605184fa231e526b1c79105d
Chris <chrischillogallid64@outlook.com>
2021-05-02 01:10:35 -0500

    feat: add operators

---
+++ clases/06-Operators/operators.js
@@ -0,0 +1,41 @@
+/* 6) ------------------ Operadores ---------------------- */
+
+/* Comparación de datos */
+/* Dos iguales (==) se comparan los valores
+Tres iguales (===) se comparan los tipos de datos y el valor */
+console.log('--------- 1 ---------');
+console.log('7'==7);
+console.log('7'===7);
+console.log(7===7);
+
+
+/* Operador (+=), incrementa  */
+/* Es como tener (i = i + 2;) igual a (i += 2;) */
+console.log('--------- 2 ---------');
+let valor1 = 1;
+valor1 += 2;
+console.log(valor1);
+
+/* Realizar cualquier operación (*=) */
+console.log('--------- 3 ---------');
+let valor2 = 8;
+valor2 *= 2;
+console.log(valor2);
+
+/* Incremento binario, como un contador en +1 */
+console.log('--------- 4 ---------');
+let valor3 = 3;
+valor3 ++;
+console.log(valor3);
+
+/* Incremento binario, lo mismo que el anterior */
+console.log('--------- 5 ---------');
+let valor4 = 2;
+++valor4;
+console.log(valor4);
+
+/* Operador (!), que denota negación */
+console.log('--------- 6 ---------');
+let valor5 = true;
+console.log(!valor5);
+/* --------------------- Operadores ---------------------- */
