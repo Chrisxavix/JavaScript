20cc243aad0a736837166b2dd98edc84954cff09
Chris <chrischillogallid64@outlook.com>
2021-08-28 20:13:50 -0500

    refactor: unknown code xd

--- ejercicios/ejercicios01.js
+++ ejercicios/ejercicios01.js
@@ -1,23 +1,17 @@
 /* -------- Ejercicios JavaScript ---------- */
 
-/* 1) Programa una función que cuente el número de 
-caracteres de una cadena de texto, 
-pe. miFuncion("Hola Mundo") devolverá 10.
+/* 1) Programa una función que cuente el número de caracteres de una cadena de texto, 
+Ejemplo: miFuncion("Hola Mundo"), devolverá 10.
 
-2) Programa una función que te devuelva el 
-texto recortado según el número de caracteres 
-indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados.
+Ejemplo: miFuncion("Hola Mundo", 4), devolverá "Hola".
 
-3) Programa una función que dada una String te 
-devuelva un Array de textos separados por cierto 
-caracter, pe. miFuncion('hola que tal', ' ') 
-devolverá ['hola', 'que', 'tal'].
+3) Programa una función que dado un String te devuelva un Array de textos separados por cierto caracter.
+Ejemplo: miFuncion('hola que tal', ' '), devolverá ['hola', 'que', 'tal'].
 
-4) Programa una función que repita un texto X 
-veces, pe. miFuncion('Hola Mundo', 3) devolverá 
-Hola Mundo Hola Mundo Hola Mundo. */
+4) Programa una función que repita un texto X veces.
+Ejemplo: miFuncion('Hola Mundo', 3), devolverá Hola Mundo Hola Mundo Hola Mundo. */
 
-/* ----------------- DESARROLLO -------------------- */
 
 /* 1) --------------------------------------------------- */
 /* Nota: entrada es un valor string, en el primer if, se 
